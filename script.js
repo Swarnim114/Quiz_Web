6bb573d23d8966d9c090d1f998f07d330a587297
Swarnim114 <hofboy04@gmail.com>
2024-05-24 21:58:15 +0530

    rewriting js and some css changes

--- script.js
+++ script.js
@@ -1,138 +1,110 @@
-const questionElement = document.getElementById('question');
+const quesContent = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
-
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
-
 let questions = [
     {
-        question: 'What is the name of the main protagonist in "Naruto"?',
-        choice1: 'Sasuke Uchiha',
-        choice2: 'Kakashi Hatake',
-        choice3: 'Naruto Uzumaki',
-        choice4: 'Itachi Uchiha',
-        answer: 3,
-    },
-    {
-        question: 'In "One Piece", what is the name of the ship used by the Straw Hat Pirates?',
-        choice1: 'Thousand Sunny',
-        choice2: 'Going Merry',
-        choice3: 'Red Force',
-        choice4: 'Moby Dick',
-        answer: 2,
-    },
-    {
-        question: 'Who is the creator of the manga "Attack on Titan"?',
-        choice1: 'Eiichiro Oda',
-        choice2: 'Hajime Isayama',
-        choice3: 'Akira Toriyama',
-        choice4: 'Tite Kubo',
-        answer: 2,
-    },
-    {
-        question: 'Which anime features a character named "Goku"?',
-        choice1: 'One Punch Man',
-        choice2: 'Dragon Ball',
-        choice3: 'Bleach',
-        choice4: 'Death Note',
-        answer: 2,
-    },
-    {
-        question: 'In "My Hero Academia", what is the hero name of Izuku Midoriya?',
-        choice1: 'Red Riot',
-        choice2: 'Deku',
-        choice3: 'Lemillion',
-        choice4: 'Shoto',
-        answer: 2,
-    },
-    {
-        question: 'Which anime features a notebook that can kill anyone whose name is written in it?',
-        choice1: 'Death Note',
-        choice2: 'Naruto',
-        choice3: 'Fullmetal Alchemist',
-        choice4: 'Tokyo Ghoul',
+        question: 'Who directed the movie "Interstellar"?',
+        choice1: 'Christopher Nolan',
+        choice2: 'Steven Spielberg',
+        choice3: 'Quentin Tarantino',
+        choice4: 'Martin Scorsese',
         answer: 1,
     },
     {
-        question: 'In "Sword Art Online", what is the name of the main protagonist?',
-        choice1: 'Kirito',
-        choice2: 'Asuna',
-        choice3: 'Sinon',
-        choice4: 'Leafa',
+        question: 'In the movie "Invictus", who portrays the role of Nelson Mandela?',
+        choice1: 'Will Smith',
+        choice2: 'Denzel Washington',
+        choice3: 'Morgan Freeman',
+        choice4: 'Forest Whitaker',
+        answer: 3,
+    },
+    {
+        question: 'Which country is the setting for the movie "La Haine"?',
+        choice1: 'United States',
+        choice2: 'France',
+        choice3: 'Italy',
+        choice4: 'Germany',
+        answer: 2,
+    },
+    {
+        question: 'What is the English title of the French film "Intouchables"?',
+        choice1: 'The Intouchables',
+        choice2: 'Untouchable',
+        choice3: 'Touching Lives',
+        choice4: 'The Touchables',
+        answer: 1,
+    },
+    {
+        question: 'Who directed the movie "The Shape of Water"?',
+        choice1: 'Guillermo del Toro',
+        choice2: 'Alfonso Cuarón',
+        choice3: 'Alejandro González Iñárritu',
+        choice4: 'Pedro Almodóvar',
         answer: 1,
     },
 ];
 
 
-// Constants
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = questions.length;
+let score = 0;
+let quesNo = 0;
+const correctMarks = 10;
+let acceptingAnswers = false;
 
-startGame = () => {
-    questionCounter = 0;
+const start = () => {
+    quesNo = 0;
     score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
+    nextQues();
 };
 
-getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        
+const nextQues = () => {
+    if (quesNo >= questions.length) {
         localStorage.setItem('mostRecentScore', score);
-        return window.location.assign('end.html');
+        window.location.href = 'end.html';
+        return;
     }
 
-    questionCounter++;
-    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-    // Update the progress bar
-    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+    progressText.textContent = `Question ${quesNo + 1} out of ${questions.length}`;
+    progressBarFull.style.width = `${((quesNo + 1) / questions.length) * 100}%`;
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    questionElement.innerText = currentQuestion.question;
+    currentQues = questions[quesNo];
+    quesContent.textContent = currentQues.question;
 
     choices.forEach(choice => {
         const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
+        choice.textContent = currentQues['choice' + number];
     });
 
-    availableQuestions.splice(questionIndex, 1);
+    quesNo++;
     acceptingAnswers = true;
 };
 
+
 choices.forEach(choice => {
-    choice.addEventListener('click', e => {
+    choice.addEventListener('click', () => {
         if (!acceptingAnswers) return;
-
         acceptingAnswers = false;
-        const selectedChoice = e.target;
+        const selectedChoice = choice;
         const selectedAnswer = selectedChoice.dataset['number'];
+        const isCorrect = selectedAnswer == currentQues.answer;
 
-        const classToApply =
-            selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-
-        if (classToApply === 'correct') {
-            incrementScore(CORRECT_BONUS);
+        if (isCorrect) {
+            updateScore(correctMarks);
         }
-
-        selectedChoice.classList.add(classToApply);
-
+        const corrOrIncorr = isCorrect ? 'correct' : 'incorrect';
+        selectedChoice.classList.add(corrOrIncorr);
         setTimeout(() => {
-            selectedChoice.classList.remove(classToApply);
-            getNewQuestion();
-        }, 1000);
+            selectedChoice.classList.remove(corrOrIncorr);
+            nextQues();
+        }, 600);
     });
 });
 
-incrementScore = num => {
+
+const updateScore = num => {
     score += num;
-    scoreText.innerText = score;
+    scoreText.textContent = score;
 };
 
-startGame();
+start();
